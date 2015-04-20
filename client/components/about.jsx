9974c3e78f52bad1abe910cbdf7a1beb406662ac
Colin Bookman <cobookman@gmail.com>
2015-04-20 11:07:25 -0700

    Simple about us page

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -3,9 +3,59 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    renderStaff: function(staff) {
+        return (
+            <div className="col-md-3 panel panel-default">
+                <div className="panel-body">
+                    <div className="author-img" style={{
+                        width: '100%',
+                        backgroundImage: 'url(' + staff.img + ')',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center'
+                    }} />
+                    <h4>{staff.name}</h4>
+                    <h5>{staff.title}</h5>
+                </div>
+            </div>
+        );
+    },
+    fixImgWidth: function() {
+        var imgs = window.document.querySelectorAll('.author-img');
+        for(var i = 0, l = imgs.length; i < l; ++i) {
+            var img = imgs[i];
+            var width = img.offsetWidth;
+            img.style.height = (width ) + 'px';
+        }
+    },
+    componentDidMount: function() {
+        this.fixImgWidth();
+    },
     render: function() {
         return (
-            <h2>About US Page</h2>
+            <div className="container">
+                <div className="row">
+                    <h2>About doyouloveus.com</h2>
+                    <p>Doyouloveus is a startup based out of The Negev hacker house located in the heart of SOMA, SF</p>
+                </div>
+                <div className="row">
+                    <h3>Staff</h3>
+                    {this.renderStaff({
+                        name: "Colin Bookman",
+                        title: "Lead Engineer",
+                        img: "/img/staff/colin.bookman.jpg"
+                    })}
+                    {this.renderStaff({
+                        name: "Brian Clark",
+                        title: "CTO",
+                        img: "/img/staff/brian.clark.jpg"
+                    })}
+                    {this.renderStaff({
+                        name: "Kumarovski Thangudic",
+                        title: "Lead Sales",
+                        img: "/img/staff/kumarovski.thangudic.jpg"
+                    })}
+                </div>
+            </div>
         );
     }
 });
