26ec12c7591d03f9de380f0e9cdde83b30eb1a02
Colin Bookman <cobookman@gmail.com>
2015-06-24 14:02:19 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,44 +5,44 @@ var config = {
         rabbitmq:  'amqp://localhost',
         twitter: {
             oauth: {
-                clientId: 'gUKpG3OEvmsvBUzqoExm6jS2Y',
-                clientSecret: 'm1duHDoyROOfuUPVU95g1mzyyq8PEw1CFucZjLT7Ys89zCjjos'
+                clientId: '******',
+                clientSecret: '*****'
             },
             cookie: {
                 password: 'cookie_encryption_password'
             },
             rateLimit: 15*1000
         },
-        stripe: 'sk_test_yT6DTIBeBaYBYTb8AsydNSgW',
+        stripe: '*****',
         mailgun: {
-            api_key: 'key-ad4db81c0a6ab3f99d617c17937fa173',
-            public_api_key: 'pubkey-c2228930a0e12d8d8894e2d75c16f0a8',
-            domain: 'sandboxf2355cabc9c64895b6bd0c8b5aa22185.mailgun.org'
+            api_key: '*****',
+            public_api_key: '*****',
+            domain: '*****'
         },
         stacklead: {
-            api_key: '53dc0886cb'
+            api_key: '*****'
         }
     },
     production: {
         rabbitmq: process.env.CLOUDAMQP_URL,
         twitter: {
             oauth: {
-                clientId: 'gUKpG3OEvmsvBUzqoExm6jS2Y',
-                clientSecret: 'm1duHDoyROOfuUPVU95g1mzyyq8PEw1CFucZjLT7Ys89zCjjos'
+                clientId: '*****',
+                clientSecret: '*****'
             },
             cookie: {
                 password: 'cookie_encryption_password'
             },
             rateLimit: 15*1000
         },
-        stripe: 'sk_live_lZzZpfKa4Tc11klJ9IyeZAC0',
+        stripe: '*****',
         mailgun: {
-            api_key: 'key-ad4db81c0a6ab3f99d617c17937fa173',
-            public_api_key: 'pubkey-c2228930a0e12d8d8894e2d75c16f0a8',
-            domain: 'doyouloveus.com'
+            api_key: '*****',
+            public_api_key: '*****',
+            domain: '*****'
         },
         stacklead: {
-            api_key: '53dc0886cb'
+            api_key: '*****'
         }
     }
 };
